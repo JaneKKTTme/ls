7234eaa95340fb22661f6e251e68591286e8d2c4
JaneKKTTme <eva.khomenko.28@mail.ru>
2017-12-20 03:09:11 +0300

    The project has been added

---
+++ main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <windows.h>
+#include <locale.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+	setlocale(LC_ALL, "Rus");
+	DIR *dr;
+	struct dirent *drn;
+	char direct[100];
+
+	if (argc==1)
+	{
+		strcpy(direct, ".");
+		dr = opendir(direct);
+		printf("Contents of directory: .\n");
+	}
+	else if (argc==2)
+	{
+		dr = opendir(argv[1]);
+		printf("Contents of directory: %s\n", argv[1]);
+	}
+	else 
+	{
+		printf("LOSER!");
+		return -1;
+	}
+	if (dr!=NULL)
+		while ((drn=readdir(dr)) != NULL)
+			printf("%s\n", drn->d_name);	
+	else
+		printf("Dir error occured.");
+	
+	closedir(dr); 
+	
+	return 0;
+}
